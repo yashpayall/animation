bcc1b38235fa4b7c943f09f4c0ba0f95546661de
yashpayall <yashpayal956@gmail.com>
2024-01-26 11:47:21 +0530

    first project

---
+++ components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <>
+      <div className="">
+        <div className="mx-w-[1440px] w-full mx-auto px-3">
+          <div className="flex justify-between"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
